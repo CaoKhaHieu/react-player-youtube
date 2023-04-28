15f87b510dd06ff1873a9186ffa3e6c106e4d462
CaoKhaHieu <caokhahieu9@gmail.com>
2023-04-28 19:18:19 +0700

    handle build app

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,25 @@ import typescript from 'rollup-plugin-typescript2';
 import cleaner from 'rollup-plugin-cleaner';
 import postcss from 'rollup-plugin-postcss';
 import alias from '@rollup/plugin-alias';
+import filesize from 'rollup-plugin-filesize';
+import progress from 'rollup-plugin-progress';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
-const inputFileName = 'index.ts';
+const input = 'index.ts';
+const output = [
+  {
+    file: 'dist/index.js',
+    format: 'cjs',
+    sourcemap: true,
+  },
+  {
+    file: 'dist/index.es.js',
+    format: 'es',
+    exports: 'named',
+    sourcemap: true,
+  },
+];
+
 const resolveAlias = {
   '@stylesheets': './src/stylesheets',
   '@components/*': ['./src/components/*'],
@@ -15,36 +31,28 @@ const resolveAlias = {
   '@hooks/*': ['./src/hooks*'],
 };
 
+const plugins = [
+  progress(),
+  babel({
+    exclude: 'node_modules/**',
+    presets: ['@babel/preset-react'],
+  }),
+  cleaner({
+    targets: ['./dist/'],
+  }),
+  typescript({
+    tsconfig: './tsconfig.json',
+  }),
+  postcss(),
+  alias({
+    entries: resolveAlias,
+  }),
+  filesize(),
+  peerDepsExternal(),
+];
+
 export default {
-  input: inputFileName,
-  output: [
-    {
-      file: 'dist/index.js',
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
-      file: 'dist/index.es.js',
-      format: 'es',
-      exports: 'named',
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    babel({
-      exclude: 'node_modules/**',
-      presets: ['@babel/preset-react'],
-    }),
-    cleaner({
-      targets: ['./dist/'],
-    }),
-    typescript({
-      tsconfig: './tsconfig.json',
-    }),
-    postcss(),
-    alias({
-      entries: resolveAlias,
-    }),
-    peerDepsExternal(),
-  ],
+  input,
+  output,
+  plugins,
 };
